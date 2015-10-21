6c0a84c49aea54558f9e98bbf223e607b001361e
A Sravan Kumar <asravan2007@gmail.com>
2015-10-21 12:33:43 +0530

    bug fix

--- lib/socialization/actors/liker.rb
+++ lib/socialization/actors/liker.rb
@@ -26,7 +26,7 @@ module Socialization
       #
       # @param [Likeable] likeable the object to be liked.
       # @return [Boolean]
-      def like!(likeable)
+      def like!(likeable, opts = {})
         raise Socialization::ArgumentError, "#{likeable} is not likeable!"  unless likeable.respond_to?(:is_likeable?) && likeable.is_likeable?
         Socialization.like_model.like!(self, likeable)
       end
